3fba78ca25bb57f67e5ad738c7a20bea215e6b6c
Vitaliy <fedyay.vitaliy@gmail.com>
2018-12-04 09:36:31 +0200

    from es5 to es6

--- homework 6/infinite carousel/main.js
+++ homework 6/infinite carousel/main.js
@@ -1,33 +1,33 @@
-var controls = document.querySelectorAll('.controls');
-for(var i = 0; i < controls.length; i++){
-  controls[i].style.display = 'inline-block';
+let controls = document.querySelectorAll('.controls')
+for(let i = 0; i < controls.length; i++){
+  controls[i].style.display = 'inline-block'
 }
 
-var slides = document.querySelectorAll('#slides .slide');
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,10000);
+let slides = document.querySelectorAll('#slides .slide')
+let currentSlide = 0
+let slideInterval = setInterval(nextSlide,10000)
 
 function nextSlide() {
-  goToSlide(currentSlide+1);
+  goToSlide(currentSlide+1)
 }
 
 function previousSlide() {
-  goToSlide(currentSlide-1);
+  goToSlide(currentSlide-1)
 }
 
 function goToSlide(n) {
-  slides[currentSlide].className = 'slide';
-  currentSlide = (n+slides.length)%slides.length;
-  slides[currentSlide].className = 'slide showing';
+  slides[currentSlide].className = 'slide'
+  currentSlide = (n+slides.length)%slides.length
+  slides[currentSlide].className = 'slide showing'
 }
 
-var playing = true;
-var next = document.getElementById('next');
-var previous = document.getElementById('previous');
+let playing = true
+let next = document.getElementById('next')
+let previous = document.getElementById('previous')
 
 next.onclick = function() {
-  nextSlide();
-};
+  nextSlide()
+}
 previous.onclick = function() {
-  previousSlide();
-};
+  previousSlide()
+}
