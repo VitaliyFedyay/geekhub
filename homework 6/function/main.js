cc01eef440407bdeee1453b08a669d869b2cf0ca
Vitaliy <fedyay.vitaliy@gmail.com>
2018-12-06 10:19:37 +0200

    function update

--- homework 6/function/main.js
+++ homework 6/function/main.js
@@ -1,13 +1,13 @@
-let number;
+let number
 
 function isPrime(number) {
-  if(number%2 === 0) {
+  if (number%2 === 0) {
     return false
   }
   else if (number == 0 || number == 1) {
     return false
   }
-  else {return true}
+  else { return true }
 }
 
 function factorial(number) {
@@ -22,17 +22,15 @@ function fib(number) {
   }
 }
 
-function isSorted() {
-  let str = prompt('enter number via comma', '')
-  str = '[' + str + ']'
-  let array = JSON.parse(str)
+function isSorted(array) {
+  let str = JSON.stringify(array)
   function compareNumeric(a, b) {
     if (a > b) return 1
     if (a < b) return -1
   }
-  if(array) {
+  if (array) {
     let check = array.sort(compareNumeric)
-    if(JSON.stringify(check) === str) {
+    if (JSON.stringify(check) === str) {
       return true
     } else {
       return false
@@ -42,26 +40,25 @@ function isSorted() {
 
 function reverse(str) {
   let arr = str.split('')
-  console.log(arr)
   let newString = []
-  for(let i = arr.length - 1; i >= 0; i--) {
+  for (let i = arr.length - 1; i >= 0; i--) {
     newString += arr[i]
   }
   return newString
 }
 
 function indexOf(array,index) {
-  for(let key in array) {
-    if(array[key] == index) {
+  for (let key in array) {
+    if (array[key] == index) {
       console.log(key)
     }
   }
 }
 
-function palindrome(str) {
+function isPalindrome(str) {
   var re = /[^A-Za-z0-9]/g;
   str = str.toLowerCase().replace(re, '');
-  var len = str.length;
+  var len = str.length
   for (var i = 0; i < len/2; i++) {
     if (str[i] !== str[len - 1 - i]) {
         return false;
@@ -69,4 +66,49 @@ function palindrome(str) {
   }
   return true;
  }
+
+ function missing(array) {
+   let arrNew = []
+
+   function compareNumeric(a, b) {
+    if (a > b) return 1
+    if (a < b) return -1
+   }
+   let arrSort = array.sort(compareNumeric)
+
+   for (let i = 0; i < arrSort.length; i++ ) {
+
+     if (arrSort[i] - arrSort[i-1] != 1) {
+       let diff = arrSort[i] - arrSort[i-1]
+       let one = 1
+
+       while (one < diff) {
+         arrNew.push(arrSort[i-1] + one)
+         one++
+       }
+     }
+   }
+   return arrNew
+ }
+
+function isBalanced(str) {
+  let arr = []
+  if (str.length <= 0) return true
+
+  for (let i = 0; i < str.length; i++) {
+    
+    if (str[i] === '{') {
+      arr.push(str[i])
+    } else if (str[i] === '}') {
+      if (arr.length > 0) {
+      arr.pop()
+    } else {
+      return false
+      }
+    }
+  }
+  if (arr.pop()) return false
+  return true
+} 
+
 
