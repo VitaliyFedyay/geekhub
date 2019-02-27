f5d104b16c31021cfbace8a9aa7c79797e03cd30
Федяй Виталий <vfedyay@hold.bayadera.ua>
2019-02-27 09:59:34 +0200

    update js

--- Homework 5/ToDo List/script.js
+++ Homework 5/ToDo List/script.js
@@ -1,27 +1,27 @@
-var list = document.querySelector('ul');
+var list = document.querySelector('ul')
 list.addEventListener('click', function (ev) {
   if(ev.target.tagName === "li") {
-    ev.target.classList.toggle('checked');
+    ev.target.classList.toggle('checked')
   } else if(ev.target.tagName === "span") {
-    var div = ev.target.parentNode;
-    div.remove();
+    var div = ev.target.parentNode
+    div.remove()
   }
-}, false);
+}, false)
 
 function newElement() {
-  var li = document.createElement('li');
-  var inputValue = document.getElementById('toDoEl').value;
-  var text = document.createTextNode(inputValue);
-  li.appendChild(text);
+  var li = document.createElement('li')
+  var inputValue = document.getElementById('toDoEl').value
+  var text = document.createTextNode(inputValue)
+  li.appendChild(text)
   if(inputValue == "") {
-    alert("enter your task");
+    alert("enter your task")
   } else {
-    document.getElementById('list').appendChild(li);
+    document.getElementById('list').appendChild(li)
   }
-  document.getElementById('toDoEl').value = "";
-  var span = document.createElement('span');
-  var txt = document.createTextNode("delete");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
+  document.getElementById('toDoEl').value = ""
+  var span = document.createElement('span')
+  var txt = document.createTextNode("delete")
+  span.className = "close"
+  span.appendChild(txt)
+  li.appendChild(span)
 }
